08cea5dad1376776d7522c8ed2ef860ed7194b9e
darknekorin <clau.alexa.castro@gmail.com>
2018-09-10 01:03:47 -0500

    Espero no Malograrla. Se agrego: Add Medicina, List Medicina, LList Medico. Ya se christian que aun no lo pruebo en apache cordova. ya lo probare luego .-.

--- js/hola.js
+++ js/hola.js
@@ -12,12 +12,36 @@ $(document).ready(function(){
     
     // MODAL ABRIR
        $('.modal').modal();
+       $('select').material_select();
     
+    
+    $('.datepicker').pickadate({
+        default: 'now',
+        firstDay: true,
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year
+        vibrate: true 
+      });
+    
+    $('.timepicker').pickatime({
+        default: 'now',
+        twelvehour: false, // change to 12 hour AM/PM clock from 24 hour
+        donetext: 'OK',
+        autoclose: false,
+        vibrate: true // vibrate the device when dragging clock hand
+    });
 
+    $("input[name$='cantdosis']").click(function() {
+          var cantDosis = $(this).val();
+        
+          $('div.timeRec').hide();
+          
+          for (var i= 1; i <= cantDosis; i++) {
+            $('#DivRecurrencia' + i).show();
+          };
+        
+    });
+    
 });
 
- 
-$(document).ready(function() {
-    $('select').material_select();
-});
-    
+
